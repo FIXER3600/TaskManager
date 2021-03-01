6721c51453ed6902172cd055086cbc4723286bb5
FIXER3600 <guilhermecomercial51@gmail.com>
2021-02-28 22:42:17 -0300

    Initial commit

---
+++ src/controller/taskcontroller.java
@@ -0,0 +1,111 @@
+package controller;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class taskcontroller {
+    public taskcontroller() {
+        super();
+    }
+
+    public String so() {
+        String so = System.getProperty("os.name");
+
+        return so;
+    }
+
+    public String readProcess(String process) {
+
+            try {
+                Process p = Runtime.getRuntime().exec(process);
+                InputStream fluxo = p.getInputStream();
+                InputStreamReader leitor = new InputStreamReader(fluxo);
+                BufferedReader buffer = new BufferedReader(leitor);
+                String linha = buffer.readLine();
+
+                while (linha != null) {
+                    System.out.println(linha);
+
+                    linha = buffer.readLine();
+                }
+                buffer.close();
+                leitor.close();
+                fluxo.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return process;
+        }
+        public void listProcess(String so) {
+            so = System.getProperty("os.name");
+            if (so.equals("Windows 10")) {
+                String process = "TASKLIST /FO TABLE";
+                try {
+                    Process p = Runtime.getRuntime().exec(process);
+                    InputStream fluxo = p.getInputStream();
+                    InputStreamReader leitor = new InputStreamReader(fluxo);
+                    BufferedReader buffer = new BufferedReader(leitor);
+                    String linha = buffer.readLine();
+
+                    while (linha != null) {
+                        System.out.println(linha);
+
+                        linha = buffer.readLine();
+                    }
+                    buffer.close();
+                    leitor.close();
+                    fluxo.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            } else if (so.equals("Linux")) {
+                String process = "ps -ef";
+            return;
+            }
+        }
+    public void killName(String so, String param) {
+        StringBuffer buffer = new StringBuffer();
+        if (so.equals("Windows 10")) {
+        String cmdNome = "TASKKILL /IM";
+            //String k = readProcess(cmdNome);
+            buffer.append(cmdNome);
+            buffer.append(" ");
+            buffer.append(param);
+        } else if (so.equals("Linux")) {
+            String cmdNome = "pkill -f" + param;
+            //String k = readProcess(cmdNome);
+            buffer.append(cmdNome);
+            buffer.append(" ");
+            buffer.append(param);
+
+        }
+        readProcess(buffer.toString());
+    }
+
+    public void killPid(String so, String param) {
+        StringBuffer buffer = new StringBuffer();
+        if (so.equals("Windows 10")) {
+            String cmdPid = "TASKKILL /PID";
+            int pid = 0;
+            pid = Integer.parseInt(param);
+            buffer.append(cmdPid);
+            buffer.append(" ");
+            buffer.append(pid);
+        }else if(so.equals("Linux")){
+            String cmdPid="kill -9"+param;
+            int pid = 0;
+            pid = Integer.parseInt(param);
+            buffer.append(cmdPid);
+            buffer.append(" ");
+            buffer.append(pid);
+        }
+        readProcess(buffer.toString());
+    }
+}
+
---
+++ src/view/Main.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.*;
+import controller.taskcontroller;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        taskcontroller procController= new taskcontroller();
+        String sistemaOper=procController.so();
+        //String so=JOptionPane.showInputDialog("Digite o nome do Sistema Operacional");
+        Integer opc=Integer.parseInt(JOptionPane.showInputDialog("1- Ver processos \n 2-Matar por nome \n 3-Matar por PID"));
+        switch (opc){
+            case 1:
+                procController.listProcess(sistemaOper);
+                break;
+            case 2:
+                    String name = JOptionPane.showInputDialog("Digite o nome da aplicação:");
+                    procController.killName(sistemaOper, name);
+                break;
+            case 3:
+                String p=JOptionPane.showInputDialog("Digite o pid da aplicação");
+
+                procController.killPid(sistemaOper,p);
+                break;
+        }
+
+
+    }
+}
